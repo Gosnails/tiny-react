795358188b5201286a56c7e00bd10bf34f6fda63
Gosnails <Fu_shide@163.com>
2024-06-10 18:00:16 +0800

    feat：更新DOM之删除节点

--- src/TinyReact/diff.js
+++ src/TinyReact/diff.js
@@ -1,23 +1,31 @@
 import createDOMElement from "./createDOMElement";
-import mountElement from "./mountElement"
+import mountElement from "./mountElement";
 import updateNodeElement from "./updateNodeElement";
-import updateTextNode from "./updateTextNode"
+import updateTextNode from "./updateTextNode";
+import unmountNode from "./unmountNode";
 
 export default function diff(virtualDOM, container, oldDOM) {
-    const oldVirtualDOM = oldDOM && oldDOM._virtualDOM;
-    if (!oldDOM) {
-        mountElement(virtualDOM, container)
-    } else if (virtualDOM.type !== oldVirtualDOM.type && typeof virtualDOM.type !== "function") {
-        const newElement = createDOMElement(virtualDOM);
-        oldDOM.parentNode.replaceChild(newElement, oldDOM)
-    } else if (oldVirtualDOM && virtualDOM.type === oldVirtualDOM.type) {
-        if (virtualDOM.type === "text") {
-            updateTextNode(virtualDOM, oldVirtualDOM, oldDOM)
-        } else {
-            updateNodeElement(oldDOM, virtualDOM, oldVirtualDOM)
-        }
-        virtualDOM.children.forEach((child, i) => {
-            diff(child, oldDOM, oldDOM.childNodes[i])
-        });
-    }
+	const oldVirtualDOM = oldDOM && oldDOM._virtualDOM;
+	if (!oldDOM) {
+		mountElement(virtualDOM, container);
+	} else if (virtualDOM.type !== oldVirtualDOM.type && typeof virtualDOM.type !== "function") {
+		const newElement = createDOMElement(virtualDOM);
+		oldDOM.parentNode.replaceChild(newElement, oldDOM);
+	} else if (oldVirtualDOM && virtualDOM.type === oldVirtualDOM.type) {
+		if (virtualDOM.type === "text") {
+			updateTextNode(virtualDOM, oldVirtualDOM, oldDOM);
+		} else {
+			updateNodeElement(oldDOM, virtualDOM, oldVirtualDOM);
+		}
+		virtualDOM.children.forEach((child, i) => {
+			diff(child, oldDOM, oldDOM.childNodes[i]);
+		});
+
+		let oldChildNodes = oldDOM.childNodes;
+		if (oldChildNodes.length > virtualDOM.children.length) {
+			for (let i = oldChildNodes.length - 1; i > virtualDOM.children.length - 1; i--) {
+				unmountNode(oldChildNodes[i]);
+			}
+		}
+	}
 }
