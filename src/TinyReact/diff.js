7d9928dcab50df6a2be05b7b3e3b8f20ce8cf566
Gosnails <Fu_shide@163.com>
2024-06-10 17:33:28 +0800

    virtualDOM对比，节点不相同

--- src/TinyReact/diff.js
+++ src/TinyReact/diff.js
@@ -1,3 +1,4 @@
+import createDOMElement from "./createDOMElement";
 import mountElement from "./mountElement"
 import updateNodeElement from "./updateNodeElement";
 import updateTextNode from "./updateTextNode"
@@ -6,6 +7,9 @@ export default function diff(virtualDOM, container, oldDOM) {
     const oldVirtualDOM = oldDOM && oldDOM._virtualDOM;
     if (!oldDOM) {
         mountElement(virtualDOM, container)
+    } else if (virtualDOM.type !== oldVirtualDOM.type && typeof virtualDOM.type !== "function") {
+        const newElement = createDOMElement(virtualDOM);
+        oldDOM.parentNode.replaceChild(newElement, oldDOM)
     } else if (oldVirtualDOM && virtualDOM.type === oldVirtualDOM.type) {
         if (virtualDOM.type === "text") {
             updateTextNode(virtualDOM, oldVirtualDOM, oldDOM)
