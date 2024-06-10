54333678abeba640e62ea16663488038bb204ec9
Gosnails <Fu_shide@163.com>
2024-06-10 17:22:00 +0800

    feat: 更新DOM之virtualDOM对比

--- src/TinyReact/updateNodeElement.js
+++ src/TinyReact/updateNodeElement.js
@@ -1,19 +1,38 @@
-export default function updateNodeElement(newElement, virtualDOM) {
-    const newProps = virtualDOM.props;
-    Object.keys(newProps).forEach(propName => {
-        const newPropsValue = newProps[propName]
-        if (propName.slice(0, 2) === "on") {
-            const eventName = propName.toLowerCase().slice(2);
-            newElement.addEventListener(eventName, newPropsValue)
-        } else if (propName === 'value' || propName === "checked") {
-            newElement[propName] = newPropsValue
-        } else if (propName !== 'children') {
-            if (propName === "className") {
-                newElement.setAttribute('class', newPropsValue)
-            } else {
-                newElement.setAttribute(propName, newPropsValue)
+export default function updateNodeElement(newElement, virtualDOM, oldVirtualDOM = {}) {
+	const newProps = virtualDOM.props;
+	const oldProps = (oldVirtualDOM.props) || {};
+	Object.keys(newProps).forEach((propName) => {
+		const newPropsValue = newProps[propName];
+		const oldPropsValue = oldProps[propName];
+		if (newPropsValue !== oldPropsValue) {
+			if (propName.slice(0, 2) === "on") {
+				const eventName = propName.toLowerCase().slice(2);
+				newElement.addEventListener(eventName, newPropsValue);
+
+				if (oldPropsValue) {
+					newElement.removeEventListener(eventName, oldPropsValue);
+				}
+			} else if (propName === "value" || propName === "checked") {
+				newElement[propName] = newPropsValue;
+			} else if (propName !== "children") {
+				if (propName === "className") {
+					newElement.setAttribute("class", newPropsValue);
+				} else {
+					newElement.setAttribute(propName, newPropsValue);
+				}
+			}
+		}
+	});
+    Object.keys(oldProps).forEach((propName) => {
+        const newPropsValue = newProps[propName];
+        const oldPropsValue = oldProps[propName];
+        if (!newPropsValue) {
+            if (propName.slice(0, 2) === "on") {
+                const eventName = propName.toLowerCase().slice(2);
+                newElement.removeEventListener(eventName, oldPropsValue)
+            } else if (propName !== "children") {
+                newElement.removeAttribute(propName);
             }
         }
     })
-
 }
--- src/index.js
+++ src/index.js
@@ -19,7 +19,29 @@ const virtualDom = (
         <input type="text" value="13" />
     </div>
 )
-// TinyReact.render(virtualDom, root)
+
+const modifyDOM = (
+    <div className="container">
+        <h1>你好 Tiny React</h1>
+        <h2 data-test="test123">(编码必杀技)</h2>
+        <div>
+            嵌套1<div>嵌套1.1</div>
+        </div>
+        <h3>(观察：这个将会被改变)</h3>
+        {2==1 && <div>如果2和1相等渲染当前内容</div>}
+        {2==2 && <div>2</div>}
+        <span>这是一段修改过的段内容</span>
+        <button onClick={() => alert("你好！！！")}>点击我</button>
+        <h3>这个将会被删除</h3>
+        2,3
+        <input type="text" />
+    </div>
+)
+TinyReact.render(virtualDom, root)
+
+setTimeout(() => {
+    TinyReact.render(modifyDOM, root)
+}, 2000);
 
 function Demo() {
     return <div>hello</div>
@@ -41,4 +63,4 @@ class Alert extends TinyReact.Component {
     }
 }
 
-TinyReact.render(<Alert title="Hello React" />, root)
+// TinyReact.render(<Alert title="Hello React" />, root)
